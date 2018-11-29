1697ebed70dca480ddc5ee59e7659c750f27187f
Robert Worth <robertworth@hotmail.com>
2018-11-29 12:32:21 +0000

    updated on samsung

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="Monkton Park Church" />
-    <Header headerText="Have I got control?" />
-    <p>Such wow. Very React.</p>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default () => (
   <div>
   <h1>Hello Gatsby!</h1>
   <p>What a world</p>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
-  <a href="/about/" target="_blank">About</a>
+  <Link to="/about/" target="_blank">About</Link>
+  <Link to="/contact/">Contact</Link>
   </div>
 )
